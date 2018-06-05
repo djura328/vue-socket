720d03c89e3fe1db836c013dc003ae21d967f72e
nikola <djura328@yahoo.com>
2018-06-05 21:34:32 +0200

    first commit

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-container grid-list-md>
+    <v-layout row wrap>
+
+      <v-flex xs12>
+        <p v-if="isConnected">We're connected to the server!</p>
+      </v-flex>
+
+      <v-flex xs10>
+        <v-text-field v-model="message" solo></v-text-field>
+      </v-flex>
+
+      <v-flex xs2>
+        <v-btn color="info" @click="emitEvent">Send</v-btn>
+      </v-flex>
+
+      <v-flex xs10>
+        <v-layout row wrap px-2>
+          <template v-for="response in responses">
+            <v-flex class="xs6 text-xs-left">{{response.message}}</v-flex>
+            <v-flex class="xs6 text-xs-right">{{response.user}}</v-flex>
+          </template>
+        </v-layout>
+      </v-flex>
+
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+    export default {
+        props: {
+          channel: {default: null}
+        },
+        data () {
+            return {
+                message: '',
+                responses: [],
+                /*channel:'channel1',*/
+                isConnected: false
+            }
+        },
+        sockets: {
+            connect() {
+                // Fired when the socket connects.
+                this.isConnected = true;
+                console.log('connect')
+            },
+
+            disconnect() {
+                this.isConnected = false;
+                console.log('disconnect')
+            },
+
+            // Fired when the server sends something on the "messageChannel" channel.
+            messageChannel(data) {
+                this.socketMessage = data
+                console.log('messageChannel')
+            }
+        },
+        created () {
+            this.$options.sockets[this.channel] = (data) => {
+                this.responses.push(data)
+                console.log('created')
+            }
+        },
+        methods: {
+            emitEvent () {
+                this.$socket.emit(this.channel, this.message)
+            }
+        },
+        watch:{
+            channel(){
+                this.$options.sockets[this.channel] = (data) => {
+                    this.responses.push(data)
+                    console.log(data)
+                }
+            }
+        }
+    }
+</script>
---
+++ server/index.js
@@ -0,0 +1,32 @@
+const server = require('http').createServer();
+
+const io = require('socket.io')(4113, {
+    path: '/',
+    serveClient: false,
+    // below are engine.IO options
+    pingInterval: 10000,
+    pingTimeout: 5000,
+    cookie: false
+});
+
+
+
+io.on("connection", client => {
+    console.log("New connection!");
+
+    client.on("channel1", newText => {
+        console.log("cchannel1!");
+        io.emit("channel1", {user:'pera', message:newText});
+    });
+
+    client.on("channel2", newText => {
+        console.log("channel2");
+        io.emit("channel2", {user:'pera', message:newText});
+    });
+
+    client.on('disconnect', function () {
+        console.log("disconnect");
+        io.emit('user disconnected');
+    });
+
+});
